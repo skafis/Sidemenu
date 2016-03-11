4abacf728231d18f90bfea83a5f8d7b6d140d2ad
skafis <franciskipchumba5@gmail.com>
2016-03-11 17:54:21 +0300

    add menu layout

--- app/src/main/java/com/example/entsfrank/myportofolio/ContentFragment.java
+++ app/src/main/java/com/example/entsfrank/myportofolio/ContentFragment.java
@@ -1,7 +1,10 @@
 package com.example.entsfrank.myportofolio;
 
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by frank on 3/11/16.
  */
-public class ContentFragment {
+public class ContentFragment extends AppCompatActivity{
+
 }
